f328020bb73e57660a7f5586ca7eb211ba6a3517
Leon Wetzel <l.wetzel@ovbureau.nl>
2020-03-08 18:43:17 +0100

    Updated documentation in script and repository

--- sample.sh
+++ sample.sh
@@ -4,9 +4,8 @@
 #
 # Usage: sh sample.sh
 
-
 # Download the wikipedia page and store it in a variable for further usage
 content=$(wget https://nl.wikipedia.org/wiki/Rijksuniversiteit_Groningen -q -O -)
 
-# Load corpus, split the corpus by spaces and put every value on a new line,  
-echo $content | tr ' ' '\n' | sed '/^$/d' | grep -w 'de' | sort | uniq -c | sort -nr 
+# Load corpus, split the corpus by spaces and put every value on a new line, search for 'de', sort by frequency
+echo $content | tr ' ' '\n' | grep -w 'de' | sort | uniq -c | sort -nr 
